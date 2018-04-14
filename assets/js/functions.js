e5bfc9b6dcfe7362361098a25d5eb39954b6dbff
Wallace <wallacevinicius@eng.ci.ufpb.br>
2018-04-14 18:23:20 -0300

    Modificações no global.js

---
+++ assets/js/functions.js
@@ -0,0 +1,133 @@
+function ShowIndices() {
+	var table = $('.black > .container > .table > tbody > tr');
+	var indice = [];
+
+	switch(id){
+		case 'riograndedonorte':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'matogrosso':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'mattogrossodosul':
+			iindice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'santacatarina':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'riograndedosul':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'saopaulo':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'minasgerais':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'riodejaneiro':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'espiritosanto':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'distritofederal':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'maranhao':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'paraiba':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'amapa':
+			indice = [[0.28],[0.63],[0.08],[0.21],[0.96],[0.92],[0.51]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}	
+			break;
+		case 'ceara':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'piaui':
+			indice = [];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'para':
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'rondonia':
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'acre':
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'amazonas': 
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'roraima':
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+		case 'tocantins':
+			indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+			for(var i = 0; i < 7; i++){
+					table.append('<td>'+indice[i]+'</td>');
+			}
+			break;
+	}
+}
--- assets/js/global.js
+++ assets/js/global.js
@@ -1,17 +1,184 @@
 $(document).ready(function(){
 	var black = $('.black'),
 	      fechar = $('#fechar')
-	      img = $('.imgWelcome');
+	      map = $('#map a');
 	black.hide();
-	$('#map a').click(function(){
-		var id = $(this).attr('id');
-		var popu = 0;
-		var title = $('.black > .title > .container > h1');
-		var desc = $('.black > .title > .container > h6');
-		var tools = $('.black > .toolbar > .container');
-		var alert = $('.black > .container > .infos');
-		var table = $('.black > .container > .table > tbody > tr');
 
+	function ShowIndices(id) {
+		var table = $('.black > .container > .table > tbody > tr');
+		var indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+
+		switch(id){
+			case 'riograndedonorte':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'matogrosso':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'mattogrossodosul':
+				table.empty();
+				iindice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'santacatarina':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'riograndedosul':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'saopaulo':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'minasgerais':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'riodejaneiro':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'espiritosanto':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'distritofederal':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'maranhao':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'paraiba':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'amapa':
+				table.empty();
+				indice = [[0.28],[0.63],[0.08],[0.21],[0.96],[0.92],[0.51]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}	
+				break;
+			case 'ceara':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'piaui':
+				table.empty();
+				indice = [[0],[0],[0],[0],[0],[0],[0],[0]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'para':
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'rondonia':
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'acre':
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'amazonas': 
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'roraima':
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+			case 'tocantins':
+				table.empty();
+				indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
+				for(var i = 0; i < 7; i++){
+						table.append('<td>'+indice[i]+'</td>');
+				}
+				break;
+		}
+	}
+
+	function ShowAlert(indiceGeral) {
+		var alert = $('.black > .container > .infos');
+
+		if(indiceGeral >= 0 && indiceGeral <= 0.3){
+			alert.html('<div class="alert alert-danger" role="alert"><i class="fas fa-exclamation-circle"></i> O indice de <strong>'+indiceGeral+'</strong> esta em uma condição péssima</div>');
+		}else if(indiceGeral > 0.3 && indiceGeral <= 0.6){
+			alert.html('<div class="alert alert-warning" role="alert"><i class="fas fa-exclamation-circle"></i> O indice de <strong>'+indiceGeral+'</strong> esta em uma condição razoavel</div>');
+		}else if(indiceGeral > 0.6 && indice <=1){
+			alert.html('<div class="alert alert-success" role="alert"><i class="fas fa-exclamation-circle"></i> O indice de <strong>'+indiceGeral+'</strong> esta em uma condição boa</div>');
+		}
+	}
+
+	function ShowPlot(id) {
+		//Variavel que recebe a população do estado
+		var popu = 0;
+
+		//Variavel que define o local de inserir o valor da população
+		var tools = $('.black > .toolbar > .container');
+
+		//Variaveis dos dados de investimento de cada estado
 		var amazonas = [["MOBILIDADE",934120],["IMPLEMENTAÇÃO DE PROJETOS/SISTEMAS",505266926.45],["PESQUISA/EDUCAÇÃO",4238538.84],["MEDICAMENTOS",8620451.32],["ESTRUTURA",35632047.96],["PROJETOS DE SANEMANTO",2614977.28]];
 		var roraima = [["MOBILIDADE",1162347.50],["IMPLEMENTAÇÃO DE PROJETOS/SISTEMAS",70101652.90],["PESQUISA/EDUCAÇÃO",49500.00],["MEDICAMENTOS",1118095.11],["ESTRUTURA",5899278.71],["PROJETOS DE SANEMANTO",1056193.08]];
 		var amapa = [["MOBILIDADE",2603080.00],["IMPLEMENTAÇÃO DE PROJETOS/SISTEMAS",120964128.87],["PESQUISA/EDUCAÇÃO",194500.00],["MEDICAMENTOS",528552.92],["ESTRUTURA",10994669.61],["PROJETOS DE SANEMANTO",3166085.20]];
@@ -20,33 +187,8 @@ $(document).ready(function(){
 		var rondonia = [["MOBILIDADE",83160.00],["IMPLEMENTAÇÃO DE PROJETOS/SISTEMAS",192555166.82],["PESQUISA/EDUCAÇÃO",1549500.00],["MEDICAMENTOS",2801875.80],["ESTRUTURA",6890661.49],["PROJETOS DE SANEMANTO",1312390.32]];
 		var acre = [["MOBILIDADE",9544315.70],["IMPLEMENTAÇÃO DE PROJETOS/SISTEMAS",201248857.50],["PESQUISA/EDUCAÇÃO",349500.00],["MEDICAMENTOS",638862.75],["ESTRUTURA",5981215.48],["PROJETOS DE SANEMANTO",2302882.50]];
 
-		var indice = [[0.02],[0.51],[0.35],[0.67],[0.60],[0.15],[0.39]];
-
-		if(id == "amazonas"){
-			if(indice[6] >= 0 && indice[6] <= 0.3){
-				alert.html('<div class="alert alert-danger" role="alert">O indice de '+indice[6]+' esta em uma condição péssima</div>');
-			}else if(indice[6] > 0.3 && indice[6] <= 0.6){
-				alert.html('<div class="alert alert-warning" role="alert">O indice de '+indice[6]+' esta em uma condição razoavel</div>');
-			}else if(indice[6] > 0.6 && indice[6] <=1){
-				alert.html('<div class="alert alert-success" role="alert">O indice de '+indice[6]+' esta em uma condição boa</div>');
-			}
-		}
-
-		for(var i = 0; i < 7; i++){
-			table.append('<td>'+indice[i]+'</td>');
-		}
-
-		// Chartkick.configure({language: "de"});
-		// Chartkick.configure({mapsApiKey: "test123"})
-		// Chartkick.options = {colors: ["#b00", "#666"]};
-		// Chartkick.options = {legend: "right"};
-		// Chartkick.options = {title: "Bingo"};
-
 		var CustomAdapter = {
 			name: "custom",
-			// renderLineChart: function (chart) {
-			//   chart.getElement().innerHTML = "Hi";
-			// },
 			renderCustomChart: function (chart) {
 			chart.getElement().innerHTML = "Custom Chart";
 			}
@@ -148,17 +290,44 @@ $(document).ready(function(){
 				break;
 		}
 
+		tools.html('<i class="fas fa-info-circle"></i> '+id+' had <span class="numPop">'+popu+' people</span> in 2015');
+	}
+
+	map.click(function(){
+		var id = $(this).attr('id');
+		var title = $('.black > .title > .container > h1');
+		var desc = $('.black > .title > .container > h6');
+		var indicesGerais = [[0.39],[0.45],[0.52],[0.21],[0.28],[0.27],[0.58]];
+
+		ShowIndices(id);
+
+		if(id == "amazonas"){
+			ShowAlert(indicesGerais[0]);
+		}else if(id == "roraima"){
+			ShowAlert(indicesGerais[1]);
+		}else if(id == "amapa"){
+			ShowAlert(indicesGerais[2]);
+		}else if(id == "para"){
+			ShowAlert(indicesGerais[3]);
+		}else if(id == "tocantins"){
+			ShowAlert(indicesGerais[4]);
+		}else if(id == "rondonia"){
+			ShowAlert(indicesGerais[5]);
+		}else if(id == "acre"){
+			ShowAlert(indicesGerais[6]);
+		}
+
+		ShowPlot(id);
+
 		title.html('<i class="fas fa-chart-area"></i> '+id+' data');
 		desc.text('Know more about '+id+' healthy');
-		tools.html('<i class="fas fa-info-circle"></i> '+id+' had <span class="numPop">'+popu+' people</span> in 2015');
 
 		black.fadeIn(500);
 	});
 	fechar.click(function() {
 		black.fadeOut(500);
-	});
-	fechar.css({
+	}).css({
 		cursor: 'pointer'
-	});
+	});;
 
 });
